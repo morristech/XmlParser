1b4da4d99783a8edb21cc39464163209f2b85879
Otto Kivikärki <ottoki@gmail.com>
2013-03-24 11:01:43 +0200

    2

--- src/fi/mysoftware/xmlparser/MainActivity.java
+++ src/fi/mysoftware/xmlparser/MainActivity.java
@@ -1,11 +1,10 @@
 package fi.mysoftware.xmlparser;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -19,14 +18,15 @@ import com.example.xmlparser.R;
  * 
  * @author Otto Kivikarki
  * @version 2013-24-3
- *
+ * 
  */
 public class MainActivity extends Activity {
 
 	private TextView textView;
 	private EditText editText;
 	private String text;
-	
+	private ArrayList<String> list;
+
 	/**
 	 * Called when program is ran.
 	 */
@@ -34,7 +34,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-			
+
+		list = new ArrayList<String>();
 		textView = (TextView) findViewById(R.id.textView1);
 		editText = (EditText) findViewById(R.id.editText1);
 	}
@@ -48,31 +49,45 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	/**
 	 * Parses xml data with given file.
 	 * 
-	 * @param text text to look for in the file
+	 * @param text
+	 *            text to look for in the file
+	 * @throws IOException
 	 */
-	public void parseData(String text) {
+	public void parseData(String text) throws IOException {
 		XmlParser xmlevent = new XmlParser();
-		BufferedInputStream bis = new BufferedInputStream(getResources().openRawResource(R.raw.sanalista));
-		ArrayList<String> list = xmlevent.parse(bis, text);
+		BufferedInputStream bis = new BufferedInputStream(getResources()
+				.openRawResource(R.raw.sanalista));
+
+		list = xmlevent.parse(bis, text);
+		String result = "";
+		for(String s : list) {
+			result = result + s + "\n";
+		}
 		if (list.size() > 0) {
-		textView.setText(list.get(0)); 
+			textView.setText(result);
 		} else {
-		textView.setText("Sanaa ei löydy");
-		}		
+			textView.setText("Sanaa ei löydy");
+		}
 	}
-	
+
 	/**
 	 * When button is pressed.
 	 * 
-	 * @param v View
+	 * @param v
+	 *            View
 	 */
 	public void findClicked(View v) {
 		text = editText.getText().toString();
-		parseData(text);
+
+		try {
+			parseData(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/fi/mysoftware/xmlparser/XmlParser.java
+++ src/fi/mysoftware/xmlparser/XmlParser.java
@@ -20,65 +20,67 @@ import android.util.Xml;
  * 
  */
 public class XmlParser {
-	
+
 	/**
 	 * Initializes a xml parser.
 	 * 
-	 * @param in InputStream in
+	 * @param in
+	 *            InputStream in
 	 * @throws XmlPullParserException
 	 * @throws IOException
 	 * @return arraylist data
 	 */
-	public ArrayList<String> parse(InputStream in, String text) {
-		
+	public ArrayList<String> parse(InputStream in, String text) throws IOException {
+
 		ArrayList<String> data = new ArrayList<String>();
-		
+
 		try {
 			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
 			factory.setNamespaceAware(true);
-			XmlPullParser parser = factory.newPullParser();		
+			XmlPullParser parser = factory.newPullParser();
 			parser.setInput(in, "UTF-8");
-			
+
 			int eventType = parser.getEventType();
-			
+
 			while (eventType != XmlPullParser.END_DOCUMENT) {
-				
+
 				String tagname = parser.getName();
-                switch (eventType) {
-                case XmlPullParser.START_TAG:
-                    if (tagname.equalsIgnoreCase("s")) {
-                        //data.add("1");
-                    }
-                    break;
- 
-                case XmlPullParser.TEXT:
-                	if(parser.getText().equals(text)) {
-                    data.add(parser.getText());
-                    Log.d("DAT", "in");
-                	}
-                    break;
- 
-                case XmlPullParser.END_TAG:
-                    if (tagname.equalsIgnoreCase("")) {
-                        
-                    } 
-                    break;
- 
-                default:
-                    break;
-                }
-                
-                eventType = parser.next();
-            }
+				
+				switch (eventType) {
+					case XmlPullParser.START_TAG:
+						if (tagname.equalsIgnoreCase("s")) {
+							// data.add("1");
+						}
+						break;
+
+					case XmlPullParser.TEXT:
+						if (parser.getText().contains(text)) {
+							data.add(parser.getText());
+							Log.d("DAT", "in");
+						}
+						break;
+
+					case XmlPullParser.END_TAG:
+						if (tagname.equalsIgnoreCase("")) {
+	
+						}
+						break;
+
+					default:
+						break;
+				}
+
+				eventType = parser.next();
+			}
+			
 		} catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } /*finally {
-			Log.d("Parser", "Method works");
-			in.close();
-		}*/
-		
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		  finally { Log.d("Parser", "Method works"); in.close(); }
+		 
+
 		return data;
 	}
 }
